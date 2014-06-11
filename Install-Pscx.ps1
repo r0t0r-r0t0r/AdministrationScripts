ee5b69092acb157a0d3daf31e75391daf590a773
r0t0r-r0t0r <soap4me@gmail.com>
2014-06-11 23:11:53 +0400

    Verbose script param activated

--- Install-Pscx.ps1
+++ Install-Pscx.ps1
@@ -1,10 +1,15 @@
+[CmdletBinding()]
+param (
+)
+
 $pscxPackage = [System.IO.Path]::GetTempFileName()
+Write-Verbose "Pscx temp file name is $pscxPackage"
 
 Write-Verbose "Downloading Pscx 3.1"
 Invoke-WebRequest -Uri http://pscx.codeplex.com/downloads/get/744915 -OutFile $pscxPackage
 
 Write-Verbose "Installing Pscx 3.1"
-Start-Job -ScriptBlock { msiexec /package $pscxPackage /quiet } | Wait-Job
+Start-Job -ScriptBlock { msiexec /package $pscxPackage /quiet /log $home/pscx.log } | Wait-Job
 
 Write-Verbose "Cleaning up temp files"
 Remove-Item $pscxPackage
